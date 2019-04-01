6a7b068785840f6ae7f1316642e89ca27ff4c9f9
Eleonora Toscano <10536582@mail.polimi.it>
2019-04-01 13:35:54 +0200

    adrenaline actions added

---
+++ src/main/java/adrenaline/AdrenalineRun.java
@@ -0,0 +1,23 @@
+package adrenaline;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class AdrenalineRun extends Run {
+
+    /**
+     * Default constructor
+     */
+    public AdrenalineRun() {
+    }
+
+    /**
+     * 
+     */
+    public void runAround() {
+        // TODO implement here
+    }
+
+}
---
+++ src/main/java/adrenaline/AdrenalineShoot.java
@@ -0,0 +1,23 @@
+package adrenaline;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class AdrenalineShoot extends Shoot {
+
+    /**
+     * Default constructor
+     */
+    public AdrenalineShoot() {
+    }
+
+    /**
+     * 
+     */
+    public void shoot() {
+        // TODO implement here
+    }
+
+}
