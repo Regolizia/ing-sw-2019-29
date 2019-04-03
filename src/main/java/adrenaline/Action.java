0a75febf3222332e8d4ff3994abadae1f3e3d97f
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-04-03 18:50:29 +0200

    Add files via upload

--- src/main/java/adrenaline/Action.java
+++ src/main/java/adrenaline/Action.java
@@ -5,4 +5,22 @@ public class Action {
 
 
 public String getName(){ return "";}
+
+public void Shoot(Player player, WeaponCard weapon)
+{
+    // /*/ref options list*/ checkPayment(Player player, WeaponCard weapon);  checkPayment is in WeaponCard
+    // /*/ref possible target list*/ canAim(Player player, // ref options list );
+    // /*/ref target list/ aimTarget(ref possible target);
+    // check if target=spawnpoint or target=player
+
+    //damage(ref target list, WeaponCard weapon, ref options list) //todo check on target in damage
+
+}
+
+public void Run(Player player /*,finalPosition*/){
+    player.getLocation(player);
+    // CHECK IF CAN RUN
+    //DO RUN
+    // RETURN NEW POS
+}
 }
