6e63d90d6acbc6dbc613167b6cfdc79ec9054bb4
Eleonora Toscano <10536582@mail.polimi.it>
2019-04-03 18:28:15 +0200

    weaponCards in Deck constructor

--- src/main/java/adrenaline/WeaponsDeck.java
+++ src/main/java/adrenaline/WeaponsDeck.java
@@ -7,24 +7,40 @@ import java.util.*;
  */
 public class WeaponsDeck extends Deck {
 
-    private LinkedList<WeaponCard> deck;
+   private LinkedList<WeaponCard> deck;
 
 
-
-
-
-    /**
-     * Default constructor
-     */
     public WeaponsDeck() {
         deck =  new LinkedList<WeaponCard>();
 
-        Cyberblade();
-        Electroscythe();
-        Flamethrower();
-        Furnace();
-
-
+        deck.add(new Cyberblade());
+        deck.add(new Electroscythe());
+        deck.add(new Flamethrower());
+        deck.add(new Furnace());
+        deck.add(new GrenadeLaucher());
+        deck.add(new Heatseeker());
+        deck.add(new Hellion());
+        deck.add(new LockRifle());
+        deck.add(new MachineGun());
+        deck.add(new PlasmaGun());
+        deck.add(new PowerGlove());
+        deck.add(new Railgun());
+        deck.add(new RocketLaucher());
+        deck.add(new Shockwave());
+        deck.add(new Shotgun());
+        deck.add(new Sledgehammer());
+        deck.add(new Thor());
+        deck.add(new TractorBeam());
+        deck.add(new VortexCannon());
+        deck.add(new Whisper());
+        deck.add(new Zx_2());
     }
+
+
+
+    
+
+
+
 
 }
