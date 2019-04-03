2a46cc5684b560dac1269269c58dd7f2c3a16cee
Regolizia <48258797+Regolizia@users.noreply.github.com>
2019-04-03 20:47:33 +0200

    ex 2

--- ing-sw-2019-29-master/src/main/java/adrenaline/PowerUpCard.java
+++ ing-sw-2019-29-master/src/main/java/adrenaline/PowerUpCard.java
@@ -1,4 +1,27 @@
 package adrenaline;
 
 public class PowerUpCard {
+    public enum PColor {
+        RED, YELLOW, GREEN
+    }
+    public PColor color;
+
+
+    public PowerUpCard(){
+
+    }
+    public PowerUpCard(Color c){
+        this.color = c;
+    }
+
+
+    public Color getColor(){
+        return this.color;
+}
+
+    //useless
+    //public void setColor(PowerUpCard card, Color c){
+      //  this.color = c ;}
+
+
 }
--- ing-sw-2019-29-master/src/main/java/adrenaline/PowerUpDeck.java
+++ ing-sw-2019-29-master/src/main/java/adrenaline/PowerUpDeck.java
@@ -7,11 +7,37 @@ import java.util.*;
  */
 public class PowerUpDeck extends Deck {
 
-    /**
-     * Default constructor
-     */
+
+
+    private LinkedList<PowerUpCard> deck;
+
+
+
+
+
+/*
+
+
+
     public PowerUpDeck() {
+        deck = new LinkedList<PowerUpCard>();
+
+        deck.add(new Newton(PColor.RED));
+        deck.add(new TagbackGrenade("YELLOW"));
+        deck.add(new TargetingScope());
+        deck.add(new Teleporter());
+
     }
+*/
+
+
+
+
+
+
+
+
+
 
     /**
      * 
