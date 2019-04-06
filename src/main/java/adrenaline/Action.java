165ebd404752440133f9b7dc26cb9d1a4a3fe5e3
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-04-06 21:55:10 +0200

    Added adrenalineOption skeleton

--- src/main/java/adrenaline/Action.java
+++ src/main/java/adrenaline/Action.java
@@ -2,38 +2,43 @@ package adrenaline;
 
 public class Action {
 
-WeaponCard weapon;
+//WeaponCard weapon;
 
 public String getName(){ return "";}
 
 
 
 public void shoot(Player player, WeaponCard weapon)
-{
+    {
+    if (player.checkDamage(player)==2)
+    {shootAdrenaline(player, weapon);
+            return;}
+
     // /*/ref options list*/ checkPayment(Player player, WeaponCard weapon);  checkPayment is in WeaponCard
     // /*/ref possible target list*/ canAim(Player player, // ref options list );
     // /*/ref target list/ aimTarget(ref possible target);
     // check if target=spawnpoint or target=player
-
     //damage(ref target list, WeaponCard weapon, ref options list) //todo check on target in damage
+    }
 
-}
+public void shootAdrenaline(Player player, WeaponCard weapon)
+    {
+        // /*/ref options list*/ checkPayment(Player player, WeaponCard weapon);  checkPayment is in WeaponCard
+        // /*/ref possible target list*/ canAim(Player player, // ref options list );
+        // /*/ref target list/ aimTarget(ref possible target);
+        // check if target=spawnpoint or target=player
+        //shoot+adrenaline option
+        //damage(ref target list, WeaponCard weapon, ref options list) //todo check on target in damage
 
-public void run(Player player /*,finalPosition*/){
-    player.getLocation(player);
-    // CHECK IF CAN RUN
-    //DO RUN
-    player.setPos(player);
-}
+    }
 
-public void grabHere(Player player){
-    player.getLocation(player);
-    // do move
-    player.setPos(player);
-}
-
-public void grabThere(Player player /*,finalPosition*/){
+public void grab(Player player /*final position*/)
+    {
         player.getLocation(player);
+        if (player.checkDamage(player)==2||player.checkDamage(player)==1) {
+            grabAdrenaline(player /*location*/);
+            return;
+            }
         // CHECK IF CAN MOVE
         //DO MOVE
         //SET POS
@@ -41,12 +46,29 @@ public void grabThere(Player player /*,finalPosition*/){
         //DO MOVE
         player.getCard(player);
     }
-    public void reload(Player player)
+public void grabAdrenaline(Player player /*final position*/)
+    {
+    // CHECK IF CAN MOVE
+    //DO MOVE
+    //SET POS
+    player.setPos(player);
+    //DO MOVE
+    player.getCard(player);
+    }
+
+public void run(Player player /*,finalPosition*/)
+    {
+    player.getLocation(player);
+    // CHECK IF CAN RUN
+    //DO RUN
+    player.setPos(player);
+    }
+public void reload(Player player)
     {
         /*weapon=*/player.getWeapon(player);
 
 
     }
-
+    /*todo frenzyShoot frenzyRun frenzyGrab*/
     }
 
