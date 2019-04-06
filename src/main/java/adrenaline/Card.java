1dc47473e9cd602b101052d062c341e323885665
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-04-06 11:34:33 +0200

    Add owner and setOwner

--- src/main/java/adrenaline/Card.java
+++ src/main/java/adrenaline/Card.java
@@ -7,21 +7,17 @@ import java.util.*;
  */
 public class Card {
 
-
+Player owner;
 
 
     /**
      * Default constructor
      */
-    public Card() {
-    }
+    public Card() {}
 
+    public void setOwner(Player player, Card card){
+    owner=player;
 
-    /**
-     * 
-     */
-    public void isTaken() {
-        // TODO implement here
     }
 
 }
