4a09c8dcbf54967ef1eca3bd0ba15b8b19f67e7e
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-04-07 18:38:41 +0200

    Added NONE to enum

--- src/main/java/adrenaline/Figure.java
+++ src/main/java/adrenaline/Figure.java
@@ -7,8 +7,8 @@ import java.util.*;
  */
 public class Figure {
 
-    public static enum PlayerColor {
-        PURPLE, YELLOW, BLUE, GREEN, GRAY;
+    protected static enum PlayerColor {
+        PURPLE, YELLOW, BLUE, GREEN, GRAY,NONE;     //NONE on the track when nobody has attacked the player
     }
     private PlayerColor color;
 
