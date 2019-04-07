e002e24daa4dde0ada814245222fe7ab1088b121
Eleonora Toscano <10536582@mail.polimi.it>
2019-04-07 19:00:51 +0200

    Implemented StartingPlayerMarker

--- src/main/java/adrenaline/StartingPlayerMarker.java
+++ src/main/java/adrenaline/StartingPlayerMarker.java
@@ -5,19 +5,25 @@ import java.util.*;
 /**
  * 
  */
-public class StartingPlayerMarker extends Player {
+public class StartingPlayerMarker{
 
+    private Player owner;
     /**
      * Default constructor
      */
     public StartingPlayerMarker() {
     }
+    public StartingPlayerMarker(Player p) {
+        this.owner = p;
+    }
 
-    /**
-     * 
-     */
-   // public void owner;
-
+    public Player getOwner(){
+        return owner;
+    }
+    // SAME AS CONSTRUCTOR
+    public void setOwner(Player p){
+        this.owner = p;
+    }
 
 
 }
