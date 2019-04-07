5f83ab02a6e0fc6cda18e9f79fdca2784a7b2f03
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-04-07 18:49:07 +0200

    Implemented Run,Grab,GrabAdrenaline

--- src/main/java/adrenaline/Action.java
+++ src/main/java/adrenaline/Action.java
@@ -35,40 +35,55 @@ public void shootAdrenaline(Player player, WeaponCard weapon)
 
     }
 
-public void grab(Player player /*final position*/)
+public void grab(Player player , int xF , int yF)
     {
-        player.getLocation(player);
-        if (player.checkDamage(player)==2||player.checkDamage(player)==1) {
-            grabAdrenaline(player /*location*/);
+
+
+        // CHECK IF CAN MOVE: move only a square up/down or a square left/right
+        if(((player.getPosX(player)-xF==1)||(player.getPosX(player)-xF==-1))&& player.getPosY(player)-yF==0)
+        {//check if there is a wall, if not
+            player.setPos(xF,yF,player);
+            //method to pickup the card
             return;
             }
-        // CHECK IF CAN MOVE
-        //DO MOVE
-        //SET POS
-        player.setPos(player);
-        //DO MOVE
-        player.getCard(player);
+        else if (player.checkDamage(player)==2||player.checkDamage(player)==1) {
+        grabAdrenaline(player,xF,yF);
+
     }
-public void grabAdrenaline(Player player /*final position*/)
-    {
-    // CHECK IF CAN MOVE
-    //DO MOVE
-    //SET POS
-    player.setPos(player);
-    //DO MOVE
-    player.getCard(player);
+    else  System.out.println("movimento non valido");
+    }
+public void grabAdrenaline(Player player, int x, int y)
+    {   //4 options: down+left down+right up+left up+ right
+        // plus two up, two down, two left, two right
+        if((player.getPosX(player)-x==2&& player.getPosY(player)-y==0)||(player.getPosX(player)-x==-2&& player.getPosY(player)-y==0)
+        ||(player.getPosX(player)-x==0&& player.getPosY(player)-y==2)||(player.getPosX(player)-x==0&& player.getPosY(player)-y==-2))
+        {//check if there is a wall, if not
+            player.setPos(x,y,player);
+            //method to pickup the card
+        }
+        if((player.getPosX(player)-x==1&& player.getPosY(player)-y==1)||(player.getPosX(player)-x==-1&& player.getPosY(player)-y==1)
+                ||(player.getPosX(player)-x==1&& player.getPosY(player)-y==-1)||(player.getPosX(player)-x==-1&& player.getPosY(player)-y==-1))
+        {//check if there is a wall, if not
+            player.setPos(x,y,player);
+            //method to pickup the card
+        }
+        else System.out.println("movimento non valido");
     }
 
-public void run(Player player /*,finalPosition*/)
+public void run(Player player, int xF,int yF)
     {
-    player.getLocation(player);
-    // CHECK IF CAN RUN
-    //DO RUN
-    player.setPos(player);
+        // can move up/down or left/right max 3 square
+       if ((player.getPosX(player)-xF==2||player.getPosX(player)-xF==-2)&&(player.getPosY(player)-yF==1||player.getPosY(player)-yF==-1))
+           player.setPos(xF,yF,player);
+       else if((player.getPosY(player)-yF==2||player.getPosY(player)-yF==-2)&&(player.getPosX(player)-xF==1||player.getPosX(player)-xF==-1))
+           player.setPos(xF,yF,player);
+       else if(((player.getPosX(player)-xF==3||player.getPosX(player)-xF==-3)&&player.getPosY(player)-yF==0)||
+               ((player.getPosY(player)-yF==3||player.getPosY(player)-yF==-3)&&player.getPosX(player)-xF==0))
+           player.setPos(xF,yF,player);
     }
 public void reload(Player player)
     {
-        /*weapon=*/player.getWeapon(player);
+        /*weapon=*/player.getWeaponCard(player);
 
     }
     /*todo frenzyShoot frenzyRun frenzyGrab*/
