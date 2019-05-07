df8d4cc3543f1fdff37dadbcaa9bb625f0ccbd4e
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-05-07 13:36:42 +0200

    added frenzy skeleton

--- src/main/java/adrenaline/FreneticAction.java
+++ src/main/java/adrenaline/FreneticAction.java
@@ -4,13 +4,30 @@ import java.util.*;
 
 /**
  * 
- */
-public class FreneticAction //extends Action {
-{
-    /**
-     * Default constructor
-     */
-    public FreneticAction() {
+ */// only with frenzy mode and at the endOfTheGame
+    //Players without damage turn over their board
+    //every player gets a freneticTorn (2xFreneticAction)
+    // look for the one with the first card
+    // if a player plays before firstPlayer chooses an action
+    // else chooses another action
+public class FreneticAction extends Action {
+
+    public FreneticAction(){
+        super();
     }
 
+
+    public boolean selectFrenzyActionBeforFirstPlayer(ActionType actionSelected,Player player,CoordinatesWithRoom c,GameBoard g,GameModel m,PayOption paymentOption){
+
+
+        return true;
+    }
+
+    public boolean selectFrenzyActionAfterFirstPlayer(ActionType actionSelected,Player player,CoordinatesWithRoom c, GameBoard g,GameModel m,PayOption paymentOption){
+        //dopo è peggio
+
+
+
+        return true;
+    }
 }
