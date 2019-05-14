ecb47557e72f25e8334546e55e372bd042425f9c
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-05-14 12:16:49 +0200

    minor fix for AmmoTile

--- src/test/java/AmmoTileTest.java
+++ src/test/java/AmmoTileTest.java
@@ -10,8 +10,8 @@ public class AmmoTileTest {
     @Test
     public void testConstructor() {
         Coordinates c1 = new Coordinates(1,2);
-        AmmoTile a = new AmmoTile(c1, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE);
-        AmmoTile b = new AmmoTile(c1, AmmoCube.CubeColor.POWERUP, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE);
+        AmmoTile a = new AmmoTile(AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE);
+        AmmoTile b = new AmmoTile(AmmoCube.CubeColor.POWERUP, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE);
         Coordinates d = a.getCoordinates();
         a.setCoordinates(2,3);
         LinkedList<AmmoCube> f = a.getAmmoTile();
