e6d71013a50c7d3980686e3a7141ca410523127b
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-05-14 12:17:24 +0200

    minor fix for AmmoTile

--- src/test/java/RoomsTest.java
+++ src/test/java/RoomsTest.java
@@ -16,14 +16,14 @@ public class RoomsTest {
         Coordinates c1 = new Coordinates(1,1);
         GameBoard board = new DeathmatchBoard();
         ((DeathmatchBoard) board).addRoom(new RoomDeath(2,2));
-        AmmoTile am = new AmmoTile(c1, AmmoCube.CubeColor.BLUE, AmmoCube.CubeColor.BLUE, AmmoCube.CubeColor.BLUE);
+        AmmoTile am = new AmmoTile(AmmoCube.CubeColor.BLUE, AmmoCube.CubeColor.BLUE, AmmoCube.CubeColor.BLUE);
         board.getRoom(0).addAmmoTile(am);
         LinkedList<AmmoTile> l = board.getRoom(0).getTiles();
 
         ((DeathmatchBoard) board).addRoom(new RoomDeath(3,2));
         ((DeathmatchBoard) board).addRoom(new RoomDeath(2,4));
 
-        board.getRoom(0).addAmmoTile(new AmmoTile(c1, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE));
+        board.getRoom(0).addAmmoTile(new AmmoTile(AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.BLUE));
         board.getRoom(1);
         board.getRoom(2);
         Room a = new Room();
