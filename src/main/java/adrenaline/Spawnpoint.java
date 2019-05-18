0090717c69085e98ae1560a32dbb08bf58adaa18
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-05-18 18:46:57 +0200

    ADD addWeaponCard

--- src/main/java/adrenaline/Spawnpoint.java
+++ src/main/java/adrenaline/Spawnpoint.java
@@ -1,16 +1,21 @@
 package adrenaline;
 
+import java.util.LinkedList;
+
 public class Spawnpoint {
 
     int x;
     int y;
+    private LinkedList<WeaponCard> weaponCards;
 
-
-    public Spawnpoint(){};
+    public Spawnpoint(){
+        weaponCards=new LinkedList<>();
+    }
 
     public Spawnpoint(int x, int y){
         this.x = x;
         this.y = y;
+        weaponCards=new LinkedList<>();
     }
 
     public int getSpawnpointX( ) {
@@ -21,5 +26,11 @@ public class Spawnpoint {
     public int getSpawnpointY( ) {
 
         return y;
+    }
+    public void addWeaponCard(WeaponCard weaponCard){
+    this.weaponCards.add(weaponCard);
+    }
+    public LinkedList<WeaponCard> getWeaponCards(){
+        return this.weaponCards;
     }
 }
