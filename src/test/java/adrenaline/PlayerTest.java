e52ff592a1f4c244eb2375570718e0da90767192
sidneymadison <48691013+sidneymadison@users.noreply.github.com>
2019-05-20 13:24:38 +0200

    added playertest

---
+++ src/test/java/adrenaline/PlayerTest.java
@@ -0,0 +1,23 @@
+package adrenaline;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    private Player player;
+
+
+    @BeforeEach
+    void setUp() {
+        CoordinatesWithRoom coordinatesWithRoom = new CoordinatesWithRoom();
+        player = new Player(coordinatesWithRoom, Figure.PlayerColor.BLUE);
+
+    }
+
+    @Test
+    void numberOfDeaths() {
+        assertEquals(player.numberOfDeaths(), 0);
+    }
+}
