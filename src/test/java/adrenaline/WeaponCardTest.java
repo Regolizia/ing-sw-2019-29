ef5620fa32de3a428b61c1f39234b55cade2b6b6
sidneymadison <48691013+sidneymadison@users.noreply.github.com>
2019-05-20 23:21:22 +0200

    Add files via upload

---
+++ src/test/java/adrenaline/WeaponCardTest.java
@@ -0,0 +1,24 @@
+package adrenaline;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeaponCardTest {
+    private WeaponCard weaponCard;
+    @BeforeEach
+    void setUp() {
+        weaponCard = new WeaponCard();
+    }
+
+    @Test
+    void setReload() {
+        weaponCard.setReload();
+
+        assertTrue(weaponCard.getReload());
+    }
+
+
+}
