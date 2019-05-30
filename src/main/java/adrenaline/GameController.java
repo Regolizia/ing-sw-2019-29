c888cfd80b1ccb12591d64f2b3b6e3b464c4902f
Krosulhah <48256812+Krosulhah@users.noreply.github.com>
2019-05-30 09:40:02 +0200

    Added player settings

--- src/main/java/adrenaline/GameController.java
+++ src/main/java/adrenaline/GameController.java
@@ -4,27 +4,44 @@ import java.util.LinkedList;
 
 public class GameController {
     private GameModel model;
-    private Server view;
-    private LinkedList<Player> players;
+    private ClientCLI viewCli;
+    private ClientGUI viewGui;
 
-    public GameController(){
-        view=new Server();
-        model=new GameModel(GameModel.Mode.DEATHMATCH, GameModel.Bot.NOBOT,view.getBoardChosen());
+    public GameController(GameModel model, ClientCLI viewCli, ClientGUI viewGui){
+       this.viewGui=viewGui;
+       this.viewCli=viewCli;
+       this.model=model;
 
     }
-
-    public void StartGame(){
-        try{ while(view.getGameIsOn()){
-
-
-
-
+//todo viewGui
+    public void playerSettingNameAndColor(String name,Figure.PlayerColor color,CoordinatesWithRoom spw){
+        if(model.getPlayers().size()<5) {
+            for (Player player2 : model.getPlayers()
+            ) {
+                if (player2.getName().equals(name)) {
+                    viewCli.sendMessage("this name is already taken");
+                    return;
+                }
+                if (player2.getColor().equals(color)) {
+                    viewCli.sendMessage("this color is already taken");
+                    return;
+                }
+                if(player2.getRespawnCoordinates().equals(spw)){
+                    viewCli.sendMessage("this spawnpoint is already taken");
+                }
             }
-
-        }catch (Error errorGameNotOn){
-
+            Player player= new Player(spw,color);
+            player.setName(name);
+            model.getPlayers().add(player);
         }
+        else viewCli.sendMessage("can't add another player");
 
     }
+
+
+    public void updateView(Player player){
+       viewCli.printPlayerDetails(player.getName(),player.returnPoints(),player.getColor());
+    }
+
 
 }
