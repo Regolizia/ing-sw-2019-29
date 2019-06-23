fd306aca4db97cd1839c4329234a08ecf960337d
Eleonora Toscano <10536582@mail.polimi.it>
2019-06-23 13:37:42 +0200

    Fixed

--- src/test/java/adrenaline/GameModelTest.java
+++ src/test/java/adrenaline/GameModelTest.java
@@ -1,27 +1,27 @@
 package adrenaline;
 
-import adrenaline.weapons.Cyberblade;
-import adrenaline.weapons.CyberbladeTest;
 import org.junit.jupiter.api.Test;
 
-import javax.management.modelmbean.ModelMBean;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameModelTest {
-GameModel model;
     @Test
     void constructor() {
+
+        GameModel model;
         model=new GameModel(GameModel.Mode.DEATHMATCH, GameModel.Bot.NOBOT,1);
         Spawnpoint s=new Spawnpoint(1,2);
         System.out.println(model.getMapUsed().getGameBoard().getRoom(2).getSpawnpoints().size());
         model.getMapUsed().getGameBoard().getRoom(2).addSpawnpoint(s);
-        
+
         System.out.println(model.getMapUsed().getGameBoard().getRoom(2).getSpawnpoints().size());
 
         System.out.println(model.getMapUsed().getGameBoard().getRoom(2).getSpawnpoints().indexOf(s));
 
-       // model.getMapUsed().getGameBoard().getRoom(1).addAmmoTile(new AmmoTile(AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED));
+        model.getMapUsed().getGameBoard().getRoom(1).addAmmoTile(new AmmoTile(AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED, AmmoCube.CubeColor.RED),1,1);
         model.populateMap();
     }
 }
